6b819843c87dc1064f0c5043d44d50ce180503c5
shivam6497 <kdmboy6397@gmail.com>
2026-07-09 16:07:37 +0000

    added middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,35 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import redisClient from "../config/redis.js";
+
+export interface AuthRequest extends Request {
+    user?: {
+        userId: string;
+        email: string;
+    };
+}
+
+export async function authMiddleware(req: AuthRequest, res: Response, next: NextFunction) {
+    const headers = req.headers.authorization;
+    if(!headers || !headers.startsWith("Bearer ")) {
+        return res.status(401).json({
+            message: "No token provided"
+        });
+    }
+
+    const token = headers.split(" ")[1] as string;
+
+    const isBlacklisted = await redisClient.get(`blacklist:${token}`);
+    if(isBlacklisted) {
+        return res.status(401).json({
+            message: "Token is blacklisted, access denied"
+        });
+    }
+
+    const secret = process.env.JWT_SECRET as string;
+
+    const decoded = jwt.verify(token, secret) as unknown as { userId: string; email: string };
+    req.user = decoded;
+    next();
+}
+ 
---
+++ src/middleware/rateLimiter.middleware.ts
@@ -0,0 +1,53 @@
+import { success } from "zod";
+import redisClient from "../config/redis.js";
+import type { Request, Response, NextFunction } from "express";
+
+const WISHLIST_IP: string[] = ["127.0.0.1", "::1"];
+
+interface RateLimitOptions {
+  maxRequests: number;
+  windowMs: number;
+}
+
+const rateLimiter = (options: RateLimitOptions) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const ip = req.ip;
+    const normalizeip = ip === "::1" ? "127.0.0.1" : ip;
+
+    if (WISHLIST_IP.includes(normalizeip as unknown as string)) {
+      next();
+      return;
+    }
+
+    const key = `rate-limit:${normalizeip}`;
+    const requests = await redisClient.incr(key);
+
+    if (requests === 1) {
+      await redisClient.expire(key, options.windowMs);
+    }
+
+    const ttl = await redisClient.ttl(key);
+
+    res.setHeader("X-RateLimit-Limit", options.maxRequests.toString());
+    res.setHeader(
+      "X-RateLimit-Remaining",
+      Math.max(options.maxRequests - requests, 0).toString(),
+    );
+
+    res.setHeader("Retry-After", ttl);
+
+    if(requests > options.maxRequests) {
+        return res.status(429).json({
+            success: false,
+            message: "Too many requests, please try again later.",
+            retryAfter: ttl,
+            limit: options.maxRequests,
+            requestMade: requests   
+        });
+    }
+
+    next();
+  };
+};
+
+export default rateLimiter;
