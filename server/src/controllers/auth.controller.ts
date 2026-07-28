3fc906bdc6a8b0ba4bc21bf05ae5096fc2643e06
shivam6497 <kdmboy6397@gmail.com>
2026-07-28 18:16:45 +0000

    fix bug

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -8,156 +8,182 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const JWT_ACCESS_SECRET = process.env.JWT_SECRET_KEY as string
-const JWT_REFRESH_SECRET= process.env.JWT_REFRESH_SECRET_KEY as string;
-
+const JWT_ACCESS_SECRET = process.env.JWT_SECRET_KEY as string;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET_KEY as string;
 
 const ACCESS_TOKEN_EXPIRES = "15m";
 const REFRESH_TOKEN_EXPIRE = "7d";
 
 const BLACKLIST_TTL = 60 * 15;
-const REFRESH_TTL = 60 * 60 * 24 * 7; 
+const REFRESH_TTL = 60 * 60 * 24 * 7;
 
 function generateAccessToken(userId: string, email: string): string {
-    return jwt.sign({ userId, email }, JWT_ACCESS_SECRET, { expiresIn: ACCESS_TOKEN_EXPIRES });
+  return jwt.sign({ userId, email }, JWT_ACCESS_SECRET, {
+    expiresIn: ACCESS_TOKEN_EXPIRES,
+  });
 }
 
 function generateRefreshToken(userId: string, email: string): string {
-    return jwt.sign({ userId, email }, JWT_REFRESH_SECRET, { expiresIn: REFRESH_TOKEN_EXPIRE });
+  return jwt.sign({ userId, email }, JWT_REFRESH_SECRET, {
+    expiresIn: REFRESH_TOKEN_EXPIRE,
+  });
 }
 
 // register endpoint
-export async function register(req: Request, res: Response, next: NextFunction): Promise<void> {
-    try {
-        const { name, email, password } = req.body;
+export async function register(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    const { name, email, password } = req.body;
 
-        const existingUser = await User.findOne({ email });
-        if(existingUser) {
-            throw new AppError("User already exists", 400);
-        }
-
-        const user = await User.create({ name, email, password });
-        const accessToken = generateAccessToken(user._id.toString(), user.email);
-        const refreshToken = generateRefreshToken(user._id.toString(), user.email);
-
-        res.cookie("refreshToken", refreshToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            maxAge: REFRESH_TTL * 1000
-        });
-
-        res.status(201).json({ 
-            success: true,
-            message: "User registered successfully",
-            accessToken,
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email
-            }
-        });
-    } catch (error) {
-       next(error);
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new AppError("User already exists", 400);
     }
+
+    const user = await User.create({ name, email, password });
+    const accessToken = generateAccessToken(user._id.toString(), user.email);
+    const refreshToken = generateRefreshToken(user._id.toString(), user.email);
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: REFRESH_TTL * 1000,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      accessToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
-
 // login endpoint
-export async function login(req: Request, res: Response, next: NextFunction): Promise<void> {
-    try {
-        const { email, password } = req.body;
+export async function login(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
-        if(!user) {
-            throw new AppError("Invalid Credentials", 400);
-        }
-
-        const isMatch = await user.comparePassword(password);
-        if(!isMatch) {
-            throw new AppError("Invalid Credentials", 400);
-        }
-
-        const accessToken = generateAccessToken(user._id.toString(), user.email);
-        const refreshToken = generateRefreshToken(user._id.toString(), user.email);
-
-        res.cookie("refreshToken", refreshToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            maxAge: REFRESH_TTL * 1000
-        });
-
-        res.status(200).json({
-            success: true,
-            message: "Login successful",
-            accessToken,
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email
-            }
-        });
-    } catch (error) {
-        next(error);
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new AppError("Invalid Credentials", 400);
     }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      throw new AppError("Invalid Credentials", 400);
+    }
+
+    const accessToken = generateAccessToken(user._id.toString(), user.email);
+    const refreshToken = generateRefreshToken(user._id.toString(), user.email);
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: REFRESH_TTL * 1000,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      accessToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 // refresh token endpoint
-export async function refreshToken(req: Request, res: Response, next: NextFunction): Promise<void> {
-    try {
-        const refreshToken = req.cookies?.refreshToken;
+export async function refreshToken(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    const refreshToken = req.cookies?.refreshToken;
 
-        if(!refreshToken) {
-            throw new AppError("No refresh Token Provided", 401);
-        }
-
-        const isBlackListed = await redisClient.get(`blacklist:${refreshToken}`);
-        if(isBlackListed) {
-            throw new AppError("Refresh Token is blacklisted, access denied", 401);
-        }
-
-        const decoded = jwt.verify(refreshToken ,JWT_REFRESH_SECRET) as {
-            userId: string;
-            email: string;
-        };
-
-        const newAccessToken = generateAccessToken(decoded.userId, decoded.email);
-
-        res.status(200).json({
-            success: true,
-            accessToken: newAccessToken,
-        });
-    } catch (error) {
-        next(error);
+    if (!refreshToken) {
+      throw new AppError("No refresh Token Provided", 401);
     }
+
+    const isBlackListed = await redisClient.get(`blacklist:${refreshToken}`);
+    if (isBlackListed) {
+      throw new AppError("Refresh Token is blacklisted, access denied", 401);
+    }
+
+    const decoded = jwt.verify(refreshToken, JWT_REFRESH_SECRET) as {
+      userId: string;
+      email: string;
+    };
+
+    const newAccessToken = generateAccessToken(decoded.userId, decoded.email);
+
+    const user = await User.findById(decoded.userId).lean();
+    if (!user) throw new AppError("User not found", 404);
+
+    res.status(200).json({
+      success: true,
+      accessToken: newAccessToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 // logout endpoint
-export async function logout(req: AuthRequest, res: Response, next: NextFunction): Promise<void> {
-    try {
-        const accessToken = req.headers.authorization?.split(" ")[1];
-        if(!accessToken) {
-            throw new AppError("No token provided", 400);
-        }
-        await redisClient.setex(`blacklist:${accessToken}`, BLACKLIST_TTL, "true");
-
-        const refreshToken = req.cookies?.refreshToken;
-        if(refreshToken) {
-            await redisClient.setex(`blacklist:${refreshToken}`, REFRESH_TTL, "true");
-        }
-        
-        res.clearCookie("refreshToken", {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-        });
-
-        res.status(200).json({
-            success: true,
-            message: "Logged out successfully"
-        });
-    } catch (error) {
-        next(error);
+export async function logout(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    const accessToken = req.headers.authorization?.split(" ")[1];
+    if (!accessToken) {
+      throw new AppError("No token provided", 400);
     }
+    await redisClient.setex(`blacklist:${accessToken}`, BLACKLIST_TTL, "true");
+
+    const refreshToken = req.cookies?.refreshToken;
+    if (refreshToken) {
+      await redisClient.setex(`blacklist:${refreshToken}`, REFRESH_TTL, "true");
+    }
+
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
 }
